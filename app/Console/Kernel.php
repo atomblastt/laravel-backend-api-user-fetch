f3a0071cca4eb14dba4b750ee2ad72a0f0833e00
Thomas Barone <thomasranjers@MacBook-Pro.local>
2024-04-06 03:56:51 +0700

    fix: cronjob schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,14 +3,18 @@
 namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
+use App\Console\Commands\Users\UserFetchDataCron;
+use App\Console\Commands\Users\UserDailyRecordCron;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('cron:fetch-user-data')->hourly();
-        $schedule->command('cron:daily-record-job')->dailyAt('23:59');
+        // $schedule->command('cron:fetch-user-data')->hourly();
+        // $schedule->command('cron:daily-record-job')->dailyAt('23:59');
+        $schedule->job(new UserFetchDataCron)->hourly();
+        $schedule->job(new UserDailyRecordCron)->dailyAt('23:59');
 
     }
 
